714ed2236bb96cb1c067935b3c31d5a247d7201e
Florent Odièvre <florentodievre@milamber.home>
2020-11-14 11:02:29 +0100

    ajout du contenu sur la page items

--- pages/blog/items.js
+++ pages/blog/items.js
@@ -10,6 +10,13 @@ const Items = () => {
       </Head>
       <Layout>
         <h1>Items</h1>
+        <ul>
+          <h2>Liste des catégories</h2>
+          <li>Culture</li>
+          <li>Géographie</li>
+          <li>Social</li>
+          <li>Economique</li>
+        </ul>
       </Layout>
     </>
   );
